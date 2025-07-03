2eae7c55c48f04cac6356d75291e61b3b50e6662
Robby Burns <burnsiv33@gmail.com>
2025-07-02 20:38:34 -0400

    made changes to post method

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+items = []
+
 @app.get("/")
 def root():
     return {"Hello": "World"}
+
+@app.post("/items")
+def create_item(item: str):
+    if item in items: # handle duplicates
+        return items
+    items.append(item)
+    return items
